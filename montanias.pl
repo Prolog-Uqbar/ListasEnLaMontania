be4ea7e039b11af27a50a90934d9898a16de324c
lspigariol <lspigariol@gmail.com>
2022-07-08 10:30:12 -0300

    consigna

--- montanias.pl
+++ montanias.pl
@@ -1,10 +1,16 @@
-tieneRefugio(catedral,frei).
-tieneRefugio(lopez,refugioLopez).
-tieneRefugio(tronador,ottoMeiling).
+tieneRefugio(catedral,frei,2012).
+tieneRefugio(lopez,refugioLopez,1983).
+tieneRefugio(tronador,ottoMeiling,2520).
+
+equipaje(juan,[20,2,1]).
+equipaje(ana,[16,1,5,8]).
+equipaje(julieta,[20]).
 
 quierenIr(tronador,[juan,pedro,ana,sofia]).
 quierenIr(lopez,[juan,carlos,julieta]).
+quierenIr(lopez,[juan,carlos]).
 quierenIr(lopez,[srPerez,sraGarcia,pequenioPereGarcia,pequeniaPerezGarcia]).
+quierenIr(catedral,[ana,sofia,juelita]).
 
 %quiereIr(tronador,juan).
 %quiereIr(tronador,pedro).
@@ -21,5 +27,8 @@ cuantosQuierenIrJuntos(Montania,Cantidad):-
     length(Grupo, Cantidad).
     
 
+%1) companieroDeExcursionAMasDeUnaMontania(Fulano,Mengano).
+%2) puedeLlegar(Fulano,Refugio)  
+%% kilos que lleva * 50 menor a la altura
 
 
