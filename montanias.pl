c0fb982ab30f5511552ececc7fae49d65f11a3c7
lspigariol <lspigariol@gmail.com>
2022-07-08 11:34:06 -0300

    soluciones

--- montanias.pl
+++ montanias.pl
@@ -5,6 +5,7 @@ tieneRefugio(tronador,ottoMeiling,2520).
 equipaje(juan,[20,2,1]).
 equipaje(ana,[16,1,5,8]).
 equipaje(julieta,[20]).
+equipaje(pedro,[20,5,6,7]).
 
 quierenIr(tronador,[juan,pedro,ana,sofia]).
 quierenIr(lopez,[juan,carlos,julieta]).
@@ -22,15 +23,52 @@ cuantosQuierenIrJuntos(Montania,Cantidad):-
     quierenIr(Montania,Grupo),
     length(Grupo, Cantidad).
     
-
 %1) companieroDeExcursionAMasDeUnaMontania(Fulano,Mengano).
+
+/*
+companieroDeExcursionAMasDeUnaMontania(Fulano,Mengano):-
+    quiereIr(Montania1, Fulano),
+    quiereIr(Montania2,Fulano),
+    quiereIr(Montania2, Mengano),
+    quiereIr(Montania1, Mengano),
+    Montania1 \=  Montania2,
+    Fulano \= Mengano.
+*/
+companieroEnMasDeUnaMontania(Fulano, Mengano):-
+    companieroDeExcursion(Montania1, Fulano, Mengano),
+    companieroDeExcursion(Montania2, Fulano, Mengano),
+    Montania1 \= Montania2.
+
+companieroDeExcursion(Montania,Persona1,Persona2):-
+    quierenIr(Montania, Grupo1),
+    member(Persona1, Grupo1),
+    member(Persona2, Grupo1),
+    Persona1 \= Persona2.
+
 %2) puedeLlegar(Fulano,Refugio)  
-%% kilos que lleva * 50 menor a la altura
+
+puedeLlegar(Fulano, Refuguio):-
+    tieneRefugio(Montania, Refuguio, Altura),
+    pesoDelEquipaje(Fulano,Peso),
+    Peso * 50 < Altura.
+
+pesoDelEquipaje(Fulano, Peso):-
+    equipaje(Fulano, Equipo),
+    sum_list(Equipo, Peso).
+
 
 %3) quien es el que lleva mas cosas?
 
+llevaMasCosas(Persona):-
+    cantidadCosasQueLLeva(Persona, Cant),
+    forall(
+        cantidadCosasQueLLeva(_, OtraCant),
+        Cant >= OtraCant
+    ).
+
+cantidadCosasQueLLeva(Persona, Cant):-
+    equipaje(Persona, ListaCosas),
+    length(ListaCosas, Cant).
+
 %4) montaniaRepetida(Montania) si
 % si existe algun grupo que vaya a esa montania y que en otro momento, todos sus integrantes vuelvan a ir juntos en otra excursion al a misma montania
-
-
-
